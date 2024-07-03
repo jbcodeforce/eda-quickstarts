95db60605fa97d871e2b9547cc832295b2ff31ae
jbcodeforce <jerome.boyer@gmail.com>
2024-07-03 11:09:40 -0700

    mv to k study

--- quarkus-kafka-producer/scripts/listTopics.sh
+++
@@ -1,1 +0,0 @@
-docker exec -ti  kafka  bash -c "/opt/kafka/bin/kafka-topics.sh --bootstrap-server kafka:29092 --list"
--- quarkus-kafka-producer/scripts/verifyOrderTopicContent.sh
+++
@@ -1,1 +0,0 @@
-docker exec -ti  kafka  bash -c "/opt/kafka/bin/kafka-console-consumer.sh --bootstrap-server kafka:29092 --topic orders --from-beginning"
